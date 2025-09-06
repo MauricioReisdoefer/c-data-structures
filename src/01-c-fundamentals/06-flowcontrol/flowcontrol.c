ebbc108a663b820855c8fb36ee40af87df0e0b90
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-06 13:47:16 -0300

    FEAT: Flowcontrol (if and else)

---
+++ src/01-c-fundamentals/06-flowcontrol/flowcontrol.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 5;
+    if (number < 10) {
+        printf("Number is lower than 10");
+    }
+    else {
+        printf("Number is bigger than 10");
+    }
+    return 0;
+}
